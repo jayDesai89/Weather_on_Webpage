561e1596b2ff89a70b5d81bd2229b7e73d08e46c
Jay Desai <jaydesai1417@gmail.com>
2021-01-01 19:52:52 -0500

    Five Day forecasted weather on view added/updated,

--- src/app/fivedayforecast/fivedayforecast.component.ts
+++ src/app/fivedayforecast/fivedayforecast.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { disconnect } from 'process';
+import { ForecastedWeather } from '../models/forcastedData';
 
 @Component({
   selector: 'app-fivedayforecast',
@@ -8,8 +9,19 @@ import { disconnect } from 'process';
 })
 export class FivedayforecastComponent implements OnInit {
   @Input() foreCastedWeatherChild;
-  some;
+  @Input() weatherUnitChild;
   allDayTemp: Array<''> = [];
+  arrayofWeatherForecasted = [];
+  forecastedDay: string;
+  forecastedDate: string;
+  dayMaxTemp : number;
+  dayMinTemp : number;
+  dayMaxHumidity : number;
+  dayFeelsLike : number;
+  dayWeatherMain : string;
+  dayWeatherDescription: string;
+  dayAvgFeelsLike: number;
+  forecastedWeather = new ForecastedWeather;
   fiveDaysForcastArray = [
     {
       'day': 'Sunday',
@@ -71,28 +83,15 @@ export class FivedayforecastComponent implements OnInit {
 
   ngOnInit() {
     console.log(this.foreCastedWeatherChild);
-    this.some = this.foreCastedWeatherChild;
-    if (this.some !== undefined) {
-      this.dayAvgWeather();
-      this.anotherdayAvgWeather(this.foreCastedWeatherChild);
+    console.log(this.weatherUnitChild);
+    if (this.foreCastedWeatherChild !== undefined) {
+      this.dayAvgWeather(this.foreCastedWeatherChild);
+      // this.anotherdayAvgWeather(this.foreCastedWeatherChild);
     }
 
   }
 
-  dayAvgWeather() {
-    this.some.list.forEach((element, index) => {
-      let pattern = /\d{4}\-\d{2}\-\d{2}/g;
-      const day = element.dt_txt.match(pattern);
-      // console.log(day);
-      // console.log(index);
-
-      if (element.dt_txt[index] === element.dt_txt[index + 1]) {
-        // this.allDayTemp.push(element.dt_txt[index]);
-      }
-    });
-  }
-
-  anotherdayAvgWeather(data) {
+  dayAvgWeather(data) {
     const groupedData = data.list.reduce((days, row) => {
       // Get only date out of full date and time string, and use that to filter each object
       const date = row.dt_txt.split(' ')[0];
@@ -115,16 +114,56 @@ export class FivedayforecastComponent implements OnInit {
      * So by the use of object.keys() method, run a loop on each key
      */
     for (let date of Object.keys(groupedData)) {
-      console.log('Date:', date);
+      console.log(groupedData[date]);
       // current date -> date
       // original items array for this date -> groupedData[date]
       console.log('RowCount:', groupedData[date].length);
-      console.log('MaxTemp:', this.getMax(groupedData[date], 'temp_max'));
-      console.log('MinTemp:', this.getMin(groupedData[date], 'temp_min'));
-      console.log('MaxHumidity:', this.getMax(groupedData[date], 'humidity'));
+      this.forecastedDay = this.getDayName(date, 'en-EN');
+      this.forecastedDate = date;
+      console.log('Date:', date);
+      console.log('Day:', this.forecastedDay);
+      this.dayMaxTemp = this.getMax(groupedData[date], 'temp_max');
+      console.log('Max_temp', this.dayMaxTemp);
+      this.dayMinTemp = this.getMin(groupedData[date], 'temp_min');
+      console.log('Min_temp', this.dayMinTemp);
+      this.dayAvgFeelsLike = this.getMin(groupedData[date], 'feels_like');
+      console.log('Avg Feels_Like', this.dayAvgFeelsLike);
+      this.dayWeatherMain = groupedData[date][0].weather[0].main;
+      this.dayWeatherDescription = groupedData[date][0].weather[0].description;
+      console.log('Weather', this.dayWeatherMain, this.dayWeatherDescription);
+
+      console.log(/n/);
+      this.arrayofWeatherForecasted.push({
+        day : this.getDayName(date, 'en-EN'),
+        forecastedDate : date,
+        dayMaxTemp : this.getMax(groupedData[date], 'temp_max'),
+        dayMinTemp : this.getMin(groupedData[date], 'temp_min'),
+        dayAvgFeelsLike : this.getMin(groupedData[date], 'feels_like'),
+        dayWeatherMain : groupedData[date][0].weather[0].main,
+        dayWeatherDescription : groupedData[date][0].weather[0].description
+      });
+
     }
+    console.log(/n/);
+    console.log(this.arrayofWeatherForecasted);
+    console.log(/n/);
   }
 
+  getDayName(dateStr, locale) {
+    let date = new Date(dateStr);
+    return date.toLocaleDateString(locale, { weekday: 'long' });
+  }
+
+  getWeatherDescription(arr, attr) {
+    // console.log(arr);
+    // let cool = arr[0].weather[0].main;
+    // console.log(cool);
+  }
+
+  getAvg(arr, attr) {
+    let getNumber = arr.map(item => item.main(attr));
+    return getNumber.reduce((a,b) => a + b) / getNumber.length;
+  }
 
   getMax(arr, attr) {
     return Math.max.apply(Math, arr.map(item => item.main[attr]));
--- src/app/models/forcastedData.ts
+++ src/app/models/forcastedData.ts
@@ -1,3 +1,19 @@
 export class ForecastedWeather {
+  public day: string;
+  public forecastedDate: Date;
+  public dayMaxTemp: number;
+  public dayMinTemp: number;
+  public dayAvgFeelsLike: number;
+  public dayWeatherMain: string;
+  public dayWeatherDescription: string;
 
+  constructor() {
+    this.day = '';
+    this.forecastedDate = new Date();
+    this.dayMaxTemp = 0;
+    this.dayMinTemp = 0;
+    this.dayAvgFeelsLike = 0;
+    this.dayWeatherMain = '';
+    this.dayWeatherDescription = '';
+  }
 }
